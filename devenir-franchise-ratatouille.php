15e7944fddef3d349e2a8376057d2732b13f317d
ngdo-pro <ngdo27@gmail.com>
2016-09-13 20:39:17 +0200

    mise en page de franchise, manque encore le formulaire

--- devenir-franchise-ratatouille.php
+++ devenir-franchise-ratatouille.php
@@ -3,14 +3,46 @@
 	include('layout/header.php');
 
 ?>
-
-<h1>Devenez franchisé Ratatouille</h1>
-
-<h2>Des possibilités de développement infinies</h2>
-
-<p>Créés en 2014, les restaurants Ratatouille se sont ouverts à la franchise en 2016, après avoir pris le temps de consolider la marque et son concept. Immédiatement reconnaissable grâce à un positionnement fort et à une approche unique - licence Disney et plat phare : la Ratatouille - Les restaurants Ratatouille se développent en France. 4 nouvelles ouvertures de restaurant sont prévues pour fin d'année 2016.</p>
-
-
+<h1 class="text-center">Devenez franchisé Ratatouille</h1>
+<section id="possibilities_franchise">
+	<h2>Des possibilités de développement infinies</h2>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-1">
+				<p>Créés en 2014, les restaurants Ratatouille se sont ouverts à la franchise en 2016, après avoir pris le temps de consolider la marque et son concept. Immédiatement reconnaissable grâce à un positionnement fort et à une approche unique - licence Disney et plat phare : la Ratatouille - Les restaurants Ratatouille se développent en France. 4 nouvelles ouvertures de restaurant sont prévues pour fin d'année 2016.</p>
+			</div>
+			<div class="col-md-10 col-md-offset-1">
+				<ul>
+					<li>Vous êtes entrepreneur</li>
+					<li>Vous avez envie de relever un challenge</li>
+					<li>Vous souhaitez vous lancer dans votre propre aventure, être autonome à la tête de votre entreprise et pouvoir vous appuyer sur une structure solide</li>
+					<li>Vous avez une bonne connaissance de votre région, du bassin d'activité dans lequel vous voulez vous implanter</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="advantages_franchise">
+	<h2>Rejoignez le réseau des restaurants Ratatouille</h2>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-3 col-md-offset-2">
+				<img src="assets/img/food.jpg" class="img-responsive">
+			</div>
+			<div class="col-md-6">
+				<ul>
+					<li>Une marque forte et cohérente, qui différencie l'enseigne de la concurrence.</li>
+					<li>Un positionnement unique grâce à un concept différent, innovant et, par définition, modulable.</li>
+					<li>Un savoir-faire éprouvé sur lequel vous pouvez compter pour vous déployer.</li>
+					<li>Des produits de qualité provenant de producteurs locaux suivant le protocole bio.</li>
+					<li>La force de travailler avec une équipe dynamique et créative.</li>
+					<li>L'opportunité de s'inscrire dans un projet ambitieux et d'y participer activement.</li>
+					<li>Une formation initiale et un accompagnement continu pour être toujours à vos côtés.</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</section>
 <?php
 
 	include('layout/footer.php');
