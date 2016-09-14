a1323cd31459610cdb2336111f3a32683cddd60e
lauriegandon <laurie.gandon@gmail.com>
2016-09-15 00:07:21 +0200

    rajout horaires restaurants et numeros de telephone et noms de villes restaurants à ouvrir

--- les-restaurants-ratatouille.php
+++ les-restaurants-ratatouille.php
@@ -8,12 +8,13 @@
 <!-- BODY -->
 <body>
 	<div class="container"> 
+	<div class="container">
 		<section id="current_restaurants">	
 			<div class="container">
 				<div class="row"> 	<!-- H1 + LOGO -->
 					<div id="top_page_restaurants">
 						<div class="col-sm-12 col-md-6">
-							<h1 class="text-left">TROUVER UN RESTAURANT RATATOUILLE</h1>
+							<h1 class="text-left">LES RESTAURANTS RATATOUILLE</h1>
 							<p class="text-left">Nos équipes vous attendent dans l'un de nos 7 restaurants.</p>	
 						</div>
 					</div>
@@ -25,52 +26,66 @@
 			<div class="container">
 				<div class="container">
 					<div class="row">
-						<div class="col-sm-4 col-md-4 col-lg-4">
+						<div class="col-sm-12 col-md- col-lg-3">
 							<div id="ratatouille_laloupe">
-								<img src="assets/img/Ratatouille-restaurant-la-loupe.jpg" alt="restaurant ratatouille ville de La Loupe" width="180" height="180">
-								<p>18 Rue de la gare <br/>28240 La Loupe<br/>0475096787</p>
+								<img src="assets/img/Ratatouille-restaurant-la-loupe.jpg" class="img-circle" alt="restaurant ratatouille ville de La Loupe" width="180" height="180">
+								<h2>Restaurant Ratatouille La Loupe</h2>
+								<p class="p_adresse_restaurant">18 Rue de la gare <br/>28240 La Loupe<br/>0475096787</p>
+								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
-						<div class="col-md-4">
+						<div class="col-sm-12 col-md- col-lg-3">
 							<div id="ratatouille_chartres">
-								<img src="assets/img/Ratatouille-restaurant-chartres.jpg" alt="restaurant ratatouille ville de Chartres" width="180" height="180">
-								<p>3 rue Charles Brune<br/>28110 Lucé<br/>0475096787</p>
+								<img src="assets/img/Ratatouille-restaurant-chartres.jpg" class="img-circle" alt="restaurant ratatouille ville de Chartres" width="180" height="180">
+								<h2>Restaurant Ratatouille Chartres</h2>
+								<p class="p_adresse_restaurant">3 rue Charles Brune<br/>28110 Lucé<br/>0475096787</p>
+								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
-						<div class="col-md-4">
+						<div class="col-sm-12 col-md- col-lg-3">
 							<div id="ratatouille_fontainebleau">
-								<img src="assets/img/Ratatouille-restaurant-fontainebleau.jpg" alt="Restaurant ratatouille ville de Fontainebleau" width="180" height="180">
-								<p>35 rue Saint Honoré<br/>77300 Fontainebleau<br/>0475096787</p>
+								<img src="assets/img/Ratatouille-restaurant-fontainebleau.jpg" class="img-circle" alt="Restaurant ratatouille ville de Fontainebleau" width="180" height="180">
+								<h2>Restaurant Ratatouille Fontainebleau</h2>
+								<p class="p_adresse_restaurant">35 rue Saint Honoré<br/>77300 Fontainebleau<br/>0475096787</p>
+								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
-						<div class="col-md-4">
+						<div class="col-sm-12 col-md- col-lg-3">
 							<div id="ratatouille_orleans">
-								<img class="img-rounded" src="assets/img/Ratatouille-restaurant-orleans.jpg" alt="Restaurant ratatouille ville de Orleans" width="180" height="180">
-								<p>1 Avenue du champ de Mars<br/>45100 Orléans<br/>0475096787</p>
+								<img class="img-circle" src="assets/img/Ratatouille-restaurant-orleans.jpg" alt="Restaurant ratatouille ville de Orleans" width="180" height="180">
+								<h2>Restaurant Ratatouille Orléans</h2>
+								<p class="p_adresse_restaurant">1 Avenue du champ de Mars<br/>45100 Orléans<br/>0475096787</p>
+								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
-						<div class="col-md-4">
+						<div class="col-sm-12 col-md- col-lg-3">
 							<div id="ratatouille_bordeaux">
-								<img class="img-rounded" src="assets/img/Ratatouille-restaurant-orleans.jpg" alt="Restaurant ratatouille ville de Orleans" width="180" height="180">
-								<p>15 Place Canteloup<br/>33800 Bordeaux<br/>0475096787</p>
+								<img class="img-circle" src="assets/img/Ratatouille-restaurant-orleans.jpg" alt="Restaurant ratatouille ville de Orleans" width="180" height="180">
+								<h2>Restaurant Ratatouille Bordeaux</h2>
+								<p class="p_adresse_restaurant">15 Place Canteloup<br/>33800 Bordeaux<br/>0475096787</p>
+								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
-						<div class="col-md-4">
+						<div class="col-sm-12 col-md- col-lg-3">
 							<div id="ratatouille_lyon">
-								<img src="assets/img/Ratatouille-restaurant-lyon.jpg" alt="restaurant ratatouille ville de Lyon" width="180" height="180">
-								<p>17 rue Delandine<br/>69002 Lyon<br/>0475096787</p>
+								<img src="assets/img/Ratatouille-restaurant-lyon.jpg" class="img-circle" alt="restaurant ratatouille ville de Lyon" width="180" height="180">
+								<h2>Restaurant Ratatouille Lyon</h2>
+								<p class="p_adresse_restaurant">17 rue Delandine<br/>69002 Lyon<br/>0475096787</p>
+								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
-						<div class="col-md-4">
+						<div class="col-sm-12 col-md- col-lg-3">
 							<div id="ratatouille_toulouse">
-								<img src="assets/img/Ratatouille-restaurant-toulouse.jpg" alt="Restaurant ratatouille ville de Toulouse" width="180" height="180">
-								<p>1 place de la Bourse <br/>31000 Toulouse<br/>0475096787</p>
+								<img src="assets/img/Ratatouille-restaurant-toulouse.jpg" class="img-circle" alt="Restaurant ratatouille ville de Toulouse" width="180" height="180">
+								<h2>Restaurant Ratatouille Toulouse</h2>
+								<p class="p_adresse_restaurant">1 place de la Bourse <br/>31000 Toulouse<br/>0475096787</p>
+								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
@@ -78,24 +93,38 @@
 				</div>
 			</div>
 		</section>
+	</div>
 
 <!--  PROCHAINES OUVERTURES -->			
 		<section id="next_restaurants">
 			<div class="container">
-				<div class="row">
-					<div class="col-sm-12 col-md-6">
-						<h2>Nos prochaines ouvertures</h2>
-						<p>texte</p>
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 col-md-6">
+							<h2 id="titreh2">Nos prochaines ouvertures de restaurants Ratatouille</h2>
+						</div>
 					</div>
-				</div>
 
 
-			<div class="container">		
-				<div class="row">
-					<div class="col-md-4">
-						<div id="ratatouille_paris">
-							<img src="assets/img/Ratatouille-restaurant-toulouse.jpg" alt="Restaurant ratatouille ville de Toulouse" width="180" height="180">
-							<p>Ouverture prochainement</p>
+				<div class="container">		
+					<div class="row">
+						<div class="col-md-4">
+							<div id="ratatouille_paris">
+								<img class="img-circle" src="assets/img/Ratatouille-restaurant-paris.jpg" alt="Restaurant ratatouille ville de Paris" width="180" height="180">
+								<p>Paris</p>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div id="ratatouille_Marseille">
+								<img class="img-circle" src="assets/img/Ratatouille-restaurant-Marseille.jpg" alt="Restaurant ratatouille ville de Toulouse" width="180" height="180">
+								<p>Marseille</p>
+							</div>
+						</div>
+						<div class="col-md-4">
+							<div id="ratatouille_paris">
+								<img class="img-circle" src="assets/img/Ratatouille-restaurant-Lille.jpg" alt="Restaurant ratatouille ville de Lille" width="180" height="180">
+								<p>Lille</p>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -103,29 +132,6 @@
 		</section>
 	</div>
 </body>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <!-- FOOTER-->
 <?php
