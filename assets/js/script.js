fb6c75d1d9f16bf4a1ef205f82346a91ec5ed9d2
lauriegandon <laurie.gandon@gmail.com>
2016-09-15 02:42:41 +0200

    ajout section h1 page restaurant la loupe en hauteur automatique

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$socialnav_height = parseInt($("#social-navbar").css('height'));
 	$mainnav_height = parseInt($("#main-nav").css('height'));
 	$section_height = $(window).height()-$mainnav_height-$socialnav_height;
-	$("#section_events, #section_franchise, #section_aboutus").css('min-height', $section_height);
+	$("#section_events, #section_franchise, #section_aboutus, #section_restaurantlaloupe").css('min-height', $section_height);
 	$("#section_events .container").css('min-height', $section_height);
 
 });
