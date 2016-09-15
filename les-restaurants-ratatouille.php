5bac68923947c7fe6f8a0b9e8c1b4d903adbb504
lauriegandon <laurie.gandon@gmail.com>
2016-09-15 02:44:13 +0200

    modification horaires

--- les-restaurants-ratatouille.php
+++ les-restaurants-ratatouille.php
@@ -31,7 +31,7 @@
 								<img src="assets/img/Ratatouille-restaurant-la-loupe.jpg" class="img-circle" alt="restaurant ratatouille ville de La Loupe" width="180" height="180">
 								<h2>Restaurant Ratatouille La Loupe</h2>
 								<p class="p_adresse_restaurant">18 Rue de la gare <br/>28240 La Loupe<br/>0475096787</p>
-								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
+								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
@@ -40,7 +40,7 @@
 								<img src="assets/img/Ratatouille-restaurant-chartres.jpg" class="img-circle" alt="restaurant ratatouille ville de Chartres" width="180" height="180">
 								<h2>Restaurant Ratatouille Chartres</h2>
 								<p class="p_adresse_restaurant">3 rue Charles Brune<br/>28110 Lucé<br/>0475096787</p>
-								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
+								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
@@ -49,7 +49,7 @@
 								<img src="assets/img/Ratatouille-restaurant-fontainebleau.jpg" class="img-circle" alt="Restaurant ratatouille ville de Fontainebleau" width="180" height="180">
 								<h2>Restaurant Ratatouille Fontainebleau</h2>
 								<p class="p_adresse_restaurant">35 rue Saint Honoré<br/>77300 Fontainebleau<br/>0475096787</p>
-								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
+								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
@@ -58,7 +58,7 @@
 								<img class="img-circle" src="assets/img/Ratatouille-restaurant-orleans.jpg" alt="Restaurant ratatouille ville de Orleans" width="180" height="180">
 								<h2>Restaurant Ratatouille Orléans</h2>
 								<p class="p_adresse_restaurant">1 Avenue du champ de Mars<br/>45100 Orléans<br/>0475096787</p>
-								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
+								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
@@ -67,7 +67,7 @@
 								<img class="img-circle" src="assets/img/Ratatouille-restaurant-orleans.jpg" alt="Restaurant ratatouille ville de Orleans" width="180" height="180">
 								<h2>Restaurant Ratatouille Bordeaux</h2>
 								<p class="p_adresse_restaurant">15 Place Canteloup<br/>33800 Bordeaux<br/>0475096787</p>
-								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
+								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
@@ -76,7 +76,7 @@
 								<img src="assets/img/Ratatouille-restaurant-lyon.jpg" class="img-circle" alt="restaurant ratatouille ville de Lyon" width="180" height="180">
 								<h2>Restaurant Ratatouille Lyon</h2>
 								<p class="p_adresse_restaurant">17 rue Delandine<br/>69002 Lyon<br/>0475096787</p>
-								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
+								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
@@ -85,7 +85,7 @@
 								<img src="assets/img/Ratatouille-restaurant-toulouse.jpg" class="img-circle" alt="Restaurant ratatouille ville de Toulouse" width="180" height="180">
 								<h2>Restaurant Ratatouille Toulouse</h2>
 								<p class="p_adresse_restaurant">1 place de la Bourse <br/>31000 Toulouse<br/>0475096787</p>
-								<p class="p_hours">Horaires d'ouverture :<br/>Lundi : 12h 21h<br/>Mardi : fermé<br/>Mercredi : 12h 21h<br/>Jeudi : 12h 21h<br/>Vendredi : 20h 23h<br/>Samedi : 20h 23h<br/>Dimanche : fermé</p>
+								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
 								<button class="btn btn-default">Accès au site</button>
 							</div>
 						</div>
