2a891dad091ee80129a646f8de44c8f27e97acad
ngdo-pro <ngdo27@gmail.com>
2016-09-15 15:23:34 +0200

    merge master

--- les-restaurants-ratatouille.php
+++ les-restaurants-ratatouille.php
@@ -4,12 +4,8 @@
 	include ('layout/header.php');
 ?>
 
-<!-- BODY -->
-<!-- BODY -->
-<body>
-	<div class="container"> 
 	<div class="container">
-		<section id="current_restaurants"  class="first_section">
+		<section id="current_restaurants" class="first_section">
 			<div class="container">
 				<div class="row"> 	<!-- H1 + LOGO -->
 					<div id="top_page_restaurants">
@@ -18,7 +14,6 @@
 							<p class="text-left">Nos équipes vous attendent dans l'un de nos 7 restaurants.</p>	
 						</div>
 					</div>
-
 				</div>
 			</div>
 
@@ -26,67 +21,67 @@
 			<div class="container">
 				<div class="container">
 					<div class="row">
-						<div class="col-sm-12 col-md- col-lg-3">
-							<div id="ratatouille_laloupe">
+						<div class="col-sm-12 col-md-4">
+							<div id="ratatouille_laloupe" class="card_restaurants">
 								<img src="assets/img/Ratatouille-restaurant-la-loupe.jpg" class="img-circle" alt="restaurant ratatouille ville de La Loupe" width="180" height="180">
-								<h2>Restaurant Ratatouille La Loupe</h2>
+								<h2 class="text-center">La Loupe</h2>
 								<p class="p_adresse_restaurant">18 Rue de la gare <br/>28240 La Loupe<br/>0475096787</p>
 								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
-								<button class="btn btn-default">Accès au site</button>
+								<div class="text-center"><button class="btn btn-default">Accès au site</button></div>
 							</div>
 						</div>
-						<div class="col-sm-12 col-md- col-lg-3">
-							<div id="ratatouille_chartres">
+						<div class="col-sm-12 col-md-4">
+							<div id="ratatouille_chartres" class="card_restaurants">
 								<img src="assets/img/Ratatouille-restaurant-chartres.jpg" class="img-circle" alt="restaurant ratatouille ville de Chartres" width="180" height="180">
-								<h2>Restaurant Ratatouille Chartres</h2>
+								<h2 class="text-center">Chartres</h2>
 								<p class="p_adresse_restaurant">3 rue Charles Brune<br/>28110 Lucé<br/>0475096787</p>
 								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
-								<button class="btn btn-default">Accès au site</button>
+								<div class="text-center"><button class="btn btn-default">Accès au site</button></div>
 							</div>
 						</div>
-						<div class="col-sm-12 col-md- col-lg-3">
-							<div id="ratatouille_fontainebleau">
+						<div class="col-sm-12 col-md-4">
+							<div id="ratatouille_fontainebleau" class="card_restaurants">
 								<img src="assets/img/Ratatouille-restaurant-fontainebleau.jpg" class="img-circle" alt="Restaurant ratatouille ville de Fontainebleau" width="180" height="180">
-								<h2>Restaurant Ratatouille Fontainebleau</h2>
+								<h2 class="text-center">Fontainebleau</h2>
 								<p class="p_adresse_restaurant">35 rue Saint Honoré<br/>77300 Fontainebleau<br/>0475096787</p>
 								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
-								<button class="btn btn-default">Accès au site</button>
+								<div class="text-center"><button class="btn btn-default">Accès au site</button></div>
 							</div>
 						</div>
-						<div class="col-sm-12 col-md- col-lg-3">
-							<div id="ratatouille_orleans">
+						<div class="col-sm-12 col-md-4">
+							<div id="ratatouille_orleans" class="card_restaurants">
 								<img class="img-circle" src="assets/img/Ratatouille-restaurant-orleans.jpg" alt="Restaurant ratatouille ville de Orleans" width="180" height="180">
-								<h2>Restaurant Ratatouille Orléans</h2>
+								<h2 class="text-center">Orléans</h2>
 								<p class="p_adresse_restaurant">1 Avenue du champ de Mars<br/>45100 Orléans<br/>0475096787</p>
 								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
-								<button class="btn btn-default">Accès au site</button>
+								<div class="text-center"><button class="btn btn-default">Accès au site</button></div>
 							</div>
 						</div>
-						<div class="col-sm-12 col-md- col-lg-3">
-							<div id="ratatouille_bordeaux">
+						<div class="col-sm-12 col-md-4">
+							<div id="ratatouille_bordeaux" class="card_restaurants">
 								<img class="img-circle" src="assets/img/Ratatouille-restaurant-orleans.jpg" alt="Restaurant ratatouille ville de Orleans" width="180" height="180">
-								<h2>Restaurant Ratatouille Bordeaux</h2>
+								<h2 class="text-center">Bordeaux</h2>
 								<p class="p_adresse_restaurant">15 Place Canteloup<br/>33800 Bordeaux<br/>0475096787</p>
 								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
-								<button class="btn btn-default">Accès au site</button>
+								<div class="text-center"><button class="btn btn-default">Accès au site</button></div>
 							</div>
 						</div>
-						<div class="col-sm-12 col-md- col-lg-3">
-							<div id="ratatouille_lyon">
+						<div class="col-sm-12 col-md-4">
+							<div id="ratatouille_lyon" class="card_restaurants">
 								<img src="assets/img/Ratatouille-restaurant-lyon.jpg" class="img-circle" alt="restaurant ratatouille ville de Lyon" width="180" height="180">
-								<h2>Restaurant Ratatouille Lyon</h2>
+								<h2 class="text-center">Lyon</h2>
 								<p class="p_adresse_restaurant">17 rue Delandine<br/>69002 Lyon<br/>0475096787</p>
 								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
-								<button class="btn btn-default">Accès au site</button>
+								<div class="text-center"><button class="btn btn-default">Accès au site</button></div>
 							</div>
 						</div>
-						<div class="col-sm-12 col-md- col-lg-3">
-							<div id="ratatouille_toulouse">
+						<div class="col-sm-12 col-md-4">
+							<div id="ratatouille_toulouse" class="card_restaurants">
 								<img src="assets/img/Ratatouille-restaurant-toulouse.jpg" class="img-circle" alt="Restaurant ratatouille ville de Toulouse" width="180" height="180">
-								<h2>Restaurant Ratatouille Toulouse</h2>
+								<h2 class="text-center">Toulouse</h2>
 								<p class="p_adresse_restaurant">1 place de la Bourse <br/>31000 Toulouse<br/>0475096787</p>
 								<p class="p_hours">Ouvert de 19h à 22h <br/><br/>Fermé les mercredi, jeudi et dimanche </p>
-								<button class="btn btn-default">Accès au site</button>
+								<div class="text-center"><button class="btn btn-default">Accès au site</button></div>
 							</div>
 						</div>
 					</div>
@@ -101,11 +96,10 @@
 				<div class="container">
 					<div class="row">
 						<div class="col-sm-12 col-md-6">
-							<h2 id="titreh2">Nos prochaines ouvertures de restaurants Ratatouille</h2>
+							<h2 class="text-center">Nos prochaines ouvertures de restaurants Ratatouille</h2>
 						</div>
 					</div>
-
-
+				</div>
 				<div class="container">		
 					<div class="row">
 						<div class="col-md-4">
@@ -131,7 +125,6 @@
 			</div>
 		</section>
 	</div>
-</body>
 
 <!-- FOOTER-->
 <?php
