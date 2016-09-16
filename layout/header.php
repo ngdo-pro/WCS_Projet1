b8d05e5817577ade67b5639b91e6e9a2bdb6246b
ngdo-pro <ngdo27@gmail.com>
2016-09-16 11:00:25 +0200

    fix navbar version mobile

--- layout/header.php
+++ layout/header.php
@@ -26,7 +26,7 @@
 					<!-- Conteneur de la navbar -->
 					<div class="container-fluid">
 						<!-- Liste des liens de la navbar-->
-						<ul class="nav navbar-nav">
+						<ul class="nav navbar-nav pull-right">
 							<li>
 								<a href="#"><strong>En</strong></a>
 							</li>
@@ -45,31 +45,33 @@
 				<!-- Seconde navbar, c'est la principale -->
 				<nav class="navbar navbar-default" id="main-nav">
 				<!-- Header de la navbar, avec le logo et le menu hamburger -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav-collapse" aria-expanded="false">
-				        <span class="sr-only">Toggle navigation</span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-				        <span class="icon-bar"></span>
-  					</button>
-  					<a class="navbar-left" href="index.php"><img src="assets/img/logo-ratatouille-header-420x115.png"></a>
-				</div>
-				<!-- partie cachée dans le menu hamburger en mode mobile, visible sur les autres support-->
-				<div class="collapse navbar-collapse" id="main-nav-collapse">
-					<ul class="nav navbar-nav">
-						<li>
-							<a id="test" href="concept-ratatouille.php">Le concept Ratatouille</a>
-						</li>
-						<li>
-							<a href="devenir-franchise-ratatouille.php">Devenir franchisé</a>
-						</li>
-						<li>
-							<a href="actualites-ratatouille.php">Nos actualités</a>
-						</li>
-						<li>
-							<a href="les-restaurants-ratatouille.php">Les restaurants Ratatouille</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
+					<div class="container-fluid">
+						<div class="navbar-header">
+							<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#main-nav-collapse" aria-expanded="false">
+								<span class="sr-only">Toggle navigation</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							</button>
+							<a class="navbar-left" href="index.php"><img src="assets/img/logo-ratatouille-header-420x115.png"></a>
+						</div>
+						<!-- partie cachée dans le menu hamburger en mode mobile, visible sur les autres support-->
+						<div class="collapse navbar-collapse" id="main-nav-collapse">
+							<ul class="nav navbar-nav pull-right">
+								<li>
+									<a href="concept-ratatouille.php" class="text-right">Le concept Ratatouille</a>
+								</li>
+								<li>
+									<a href="devenir-franchise-ratatouille.php" class="text-right">Devenir franchisé</a>
+								</li>
+								<li>
+									<a href="actualites-ratatouille.php" class="text-right">Nos actualités</a>
+								</li>
+								<li>
+									<a href="les-restaurants-ratatouille.php" class="text-right">Les restaurants Ratatouille</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
 			</div>
